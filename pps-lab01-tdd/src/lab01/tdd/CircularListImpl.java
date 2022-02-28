dd736300860f8089286fdddc4b907a8bd3132934
LeonardoMicelli <leonardo.micelli@studio.unibo.it>
2022-02-28 12:03:57 +0100

    add test add, size and empty

---
+++ pps-lab01-tdd/src/lab01/tdd/CircularListImpl.java
@@ -0,0 +1,48 @@
+package lab01.tdd;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class CircularListImpl implements CircularList {
+    private List<Integer> list;
+
+    public CircularListImpl(){
+        this.list = new ArrayList<>();
+    }
+
+    @Override
+    public void add(int element) {
+        list.add(element);
+    }
+
+    @Override
+    public int size() {
+        return list.size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return list.isEmpty();
+    }
+
+    @Override
+    public Optional<Integer> next() {
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<Integer> previous() {
+        return Optional.empty();
+    }
+
+    @Override
+    public void reset() {
+
+    }
+
+    @Override
+    public Optional<Integer> next(SelectStrategy strategy) {
+        return Optional.empty();
+    }
+}
--- pps-lab01-tdd/test/CircularListTest.java
+++ pps-lab01-tdd/test/CircularListTest.java
@@ -1,17 +1,36 @@
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.Test;
+import lab01.tdd.CircularList;
+import lab01.tdd.CircularListImpl;
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * The test suite for testing the CircularList implementation
  */
 public class CircularListTest {
+    private CircularList cl;
 
-    //TODO: test implementation
+    @BeforeEach
+    void setup(){
+        cl = new CircularListImpl();
+    }
 
-    @Disabled
-    @Test public void testTodo(){
-        Assertions.fail();
+    @Test
+    void testIsEmpty(){
+        assertTrue(cl.isEmpty());
+    }
+
+    @Test
+    void testAdd(){
+        cl.add(1);
+        assertFalse(cl.isEmpty());
+    }
+
+    @Test
+    void testSize(){
+        cl.add(1);
+        cl.add(2);
+        assertEquals(2,cl.size());
     }
 
 }
